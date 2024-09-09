6b5c9e61c2beeb33c51bcadf1f9de31afadd6f00
LaibaAbdulhaq <laibaabdulhaq39@gmail.com>
2024-09-09 17:46:10 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function toggleSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.style.display = section.style.display === 'none' ? 'block' : 'none';
+    }
+}
---
+++ script.ts
@@ -0,0 +1,6 @@
+function toggleSection(sectionId: string): void {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.style.display = section.style.display === 'none' ? 'block' : 'none';
+    }
+}
